9cda4cb685af7a0aace402012a5fe30676bc3d7e
Stephan Beeler <beelerstephan@gmail.com>
2017-03-10 15:16:23 +0100

    add twig extension

---
+++ src/Twig/HashidsExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Roukmoute\HashidsBundle\Twig;
+
+use Roukmoute\HashidsBundle\Hashids;
+
+class HashidsExtension extends \Twig_Extension
+{
+    /**
+     * @var \Hashids\Hashids;
+     */
+    private $hashids;
+
+    public function __construct(Hashids $hashids)
+    {
+        $this->hashids = $hashids;
+    }
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('hashids_encode', [$this, 'encode']),
+            new \Twig_SimpleFilter('hashids_decode', [$this, 'decode']),
+        ];
+    }
+
+    public function getName()
+    {
+        return 'hashids_extension';
+    }
+
+    public function encode($number)
+    {
+        return $this->hashids->encode($number);
+    }
+
+    public function decode($hash)
+    {
+        return $this->hashids->decode($hash);
+    }
+
+}
