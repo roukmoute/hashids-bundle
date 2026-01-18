411f9ab61bfcea1a15f653699b75c8495f16de02
Mathias STRASSER <contact@roukmoute.fr>
2026-01-18 02:39:21 +0100

    Fix CI: use spread operator for iterable, use ^6.4/^7.0 constraints

--- spec/ValueResolver/HashidsValueResolverSpec.php
+++ spec/ValueResolver/HashidsValueResolverSpec.php
@@ -40,7 +40,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('parameterId', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_hashes_when_id_is_in_request(): void
@@ -49,7 +49,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('id', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_prevents_double_decode_for_aliases(HashidsInterface $hashids): void
@@ -60,11 +60,11 @@ class HashidsValueResolverSpec extends ObjectBehavior
 
         $argumentId = new ArgumentMetadata('id', 'int', false, false, null);
         $result = $this->resolve($request, $argumentId);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
 
         $argumentHashid = new ArgumentMetadata('hashid', 'int', false, false, null);
         $result = $this->resolve($request, $argumentHashid);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([]);
+        expect([...$result->getWrappedObject()])->toBe([]);
     }
 
     public function it_hashes_when_variable_in_request_is_prefixed(): void
@@ -73,7 +73,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('post', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_hashes_variable_in_request_when_auto_convert_is_activated(HashidsInterface $hashids): void
@@ -85,7 +85,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('post', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_does_not_hash_variable_in_request_when_auto_convert_is_deactivated(): void
@@ -94,7 +94,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('post', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([]);
+        expect([...$result->getWrappedObject()])->toBe([]);
     }
 
     public function it_throws_when_force_hash_variable_in_request_and_auto_convert_is_deactivated(HashidsInterface $hashids): void
@@ -114,7 +114,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([]);
+        expect([...$result->getWrappedObject()])->toBe([]);
     }
 
     public function it_returns_value_when_passthrough_is_false(): void
@@ -123,7 +123,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('parameterId', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_sets_attribute_and_returns_empty_when_passthrough_is_true(HashidsInterface $hashids): void
@@ -135,7 +135,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('parameterId', 'int', false, false, null);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([]);
+        expect([...$result->getWrappedObject()])->toBe([]);
         expect($request->attributes->get('parameterId'))->toBe(42);
     }
 
@@ -148,7 +148,7 @@ class HashidsValueResolverSpec extends ObjectBehavior
         $argument = new ArgumentMetadata('post', 'int', false, false, null, false, [new Hashid()]);
 
         $result = $this->resolve($request, $argument);
-        expect(iterator_to_array($result->getWrappedObject()))->toBe([42]);
+        expect([...$result->getWrappedObject()])->toBe([42]);
     }
 
     public function it_throws_when_hashid_attribute_present_and_decode_fails(HashidsInterface $hashids): void
